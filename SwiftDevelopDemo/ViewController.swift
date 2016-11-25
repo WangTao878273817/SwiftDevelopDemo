b1b993f2261e995bfb57dbc71d92e181ccc7b837
WangTao <878273817@qq.com>
2016-11-25 15:00:24 +0800

    SwiftDemo
    第一次上传这个文件到github

---
+++ SwiftDevelopDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SwiftDevelopDemo
+//
+//  Created by shoule on 2016/11/24.
+//  Copyright © 2016年 SaiDicaprio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bgImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
